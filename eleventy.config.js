8f0dea3772d106c51f823473457a46442293bdb8
NyxTheMage <nyx@nyxthemage.com>
2026-09-04 12:32:47 +1000

    From author to public workflow.
    I just hate the word content.

--- eleventy.config.js
+++ eleventy.config.js
@@ -14,7 +14,7 @@ export default async function(eleventyConfig) {
 
 export const config = {
   dir: {
-    input: "./content",
+    input: "./author",
     data: "./data",
     includes: "./includes",
     output: "./public"
